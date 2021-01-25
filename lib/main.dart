32f0adac95d0ed61add83a79de02f6e6bf2c5de2
Benoit Pingris <benoit.pingris@pm.me>
2021-01-25 15:44:46 +0100

    bruh bruh title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tabatapp',
       initialRoute: '/menu',
       routes: {
         '/menu': (context) => MenuScreen(),
